6e87e8aa940d16e942eb3f2153d540b99212573c
kanhaiya lal <kanhaiyalalkesar@gmail.com>
2021-08-08 22:47:05 +0530

    ji

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path")
 const app = express()
-const port = 2000;
+const port = process.env.PORT || 2000;
 const hbs = require("hbs")
 
 const static_path = path.join(__dirname, "../public")
